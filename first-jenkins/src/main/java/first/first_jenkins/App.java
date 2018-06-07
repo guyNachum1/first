f3392cdf5c52fd310e262e1c6949b8f01fe217be
Guy Nachum <Guyn@sapiens.com>
2018-06-08 02:07:55 +0300

    scdsac

--- first-jenkins/src/main/java/first/first_jenkins/App.java
+++ first-jenkins/src/main/java/first/first_jenkins/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!-Commit 2")ds;
+        System.out.println( "Hello World!-Commit 2");
     }
 }
